0c20f028ba1b586f177b59f7b1b0ce302974a0fd
Paul Lowrance <tpaul.lowrance@gmail.com>
2022-12-10 07:17:54 -0600

    Day 6 part 2 solution ... rust windows are great!

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -2,6 +2,20 @@ fn main() {
     let input = include_str!("./day6.prod");
     let part_one_solution = part_one(input);
     println!("Part One Solution: {part_one_solution}");
+    let part_two_solution = part_two(input);
+    println!("Part Two Solution: {part_two_solution}");
+}
+
+fn part_two(in_str: &str) -> i32 {
+    let mut counter = 0;
+    for window in in_str.chars().collect::<Vec<char>>().windows(14) {
+        counter += 1;
+        match is_str_unique(window) {
+            Some((first_idx, second_idx, ch)) => println!("Evaluating '{:?}'", window),
+            None => return counter + 13,
+        }
+    }
+    return 0;
 }
 
 fn part_one(in_str: &str) -> i32 {
