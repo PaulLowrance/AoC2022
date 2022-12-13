3fccf59df0c551eb4c477fd7009ba3ff15dbf2ba
Paul Lowrance <tpaul.lowrance@gmail.com>
2022-12-13 07:02:26 -0600

    Day 7 part 2 solution and refactor for reused of iterations

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -3,11 +3,28 @@ use std::{collections::HashMap, path::PathBuf};
 fn main() {
     let input = include_str!("./day7.prod");
     let p1_result = part_one(input);
+    let p2_result = part_two(input);
 
     println!("Part One Result: {p1_result}");
+    println!("Part Two Result: {p2_result}");
 }
 
 fn part_one(input: &str) -> i32 {
+    let dir_sizes = get_sizes(input);
+    return dir_sizes.into_values().filter(|size| *size <= 100_000).sum();
+}
+
+fn part_two(input: &str) -> i32 {
+    let dir_sizes = get_sizes(input);
+    let max_size = 70_000_000;
+    let min_size = 30_000_000;
+    let root = dir_sizes.get(&PathBuf::from("/")).unwrap();
+    let available = max_size - root;
+
+    return dir_sizes.into_values().filter(|s| available + s >= min_size).min().unwrap();
+}
+
+fn get_sizes(input: &str) -> HashMap<PathBuf, i32> {
     let mut dir_sizes = HashMap::new();
     let mut directories = Vec::new();
 
@@ -33,5 +50,5 @@ fn part_one(input: &str) -> i32 {
             _ => {}
         };
     }
-    return dir_sizes.into_values().filter(|size| *size <= 100000).sum();
+    return dir_sizes;
 }
